3f6f0c4a3344e3f06445ac0e7adc32eb5b493deb
Rubens <rubens.meirelles@postalsaude.com.br>
2024-10-30 22:26:16 -0300

    Complemento do commit anterior

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -5,5 +5,25 @@
 
     <a href="{{ route('courses.show') }}">Visualizar</a>
     <a href="{{ route('courses.create') }}">Cadastrar</a>
+
+    <table>
+        <tr>
+            <thead></thead>
+        </tr>
+    </table>
+
+    @forelse ($courses as $course)
+        {{ $course->id }}<br>
+        {{ $course->name }}<br>
+        {{ \Carbon\carbon::parse($course->created_at)->format('d/m/y H:i:s') }}<br>
+        {{ \Carbon\carbon::parse($course->updated_at)->format('d/m/y H:i:s') }}<br>
+
+        <hr>
+    @empty
+        <p style="color: #f00">Nenhum registro encontrado!</p>
+    @endforelse
+
+    {{-- Exibir a paginaçã --}}
+    {{-- {{ $courses->links() }} --}}
 @endsection
 

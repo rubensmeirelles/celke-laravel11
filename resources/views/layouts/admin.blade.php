3063bcdab228d96159ddb5c362cb6da12b4916ee
Rubens <rubens.meirelles@postalsaude.com.br>
2024-11-28 22:19:40 -0300

    Incluindo bootstrap no projeto

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <title>Celke</title>
 </head>
 <body>
    @yield('content')
+
+   <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>

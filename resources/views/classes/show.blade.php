9c3bfe2d8427ba4b18724eda35498962b3624fdb
Rubens <rubens.meirelles@postalsaude.com.br>
2024-11-29 16:44:08 -0300

    Implementado o layout de visualizar o curso e a aula

--- resources/views/classes/show.blade.php
+++ resources/views/classes/show.blade.php
@@ -1,24 +1,54 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>Detalhes da aula - {{ $classe->name }}</h2>
+<div class="container-fluid px-4">
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Detalhes da aula - {{ $classe->name }}</h2>
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item active">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">Aula</li>
+        </ol>
+    </div>
 
-    <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}">
-        <button>Listar aulas</button>
-    </a><br><br>
-    <a href="{{ route('classe.edit', ['classe' => $classe->id]) }}"><button>Editar</button></a><br><br>
+    <div class="card mb-4 hstack gap-2">
+        <span class="card-header">Visualizar</span>
+        <span class="ms-auto">
+            <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="btn btn-info btn-sm">Aulas</a>
+            <a href="{{ route('classe.index', ['course' => $classe->course_id]) }}" class="btn btn-secondary btn-sm">Listar</a>
+        </span>
+    </div>
 
-    <x-alert />
+    <div class="card-body">
+        <x-alert />
 
-    <form action="{{ route('classe.destroy', ['classe' => $classe->id]) }}" method="POST">
-        @csrf
-        @method('delete')
-        <button type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
-    </form>
+        <dl class="row">
+            <dt class="col-sm-3">Id</dt>
+            <dd class="col-sm-9">{{ $classe->id }}</dd>
 
-    Aula: {{ $classe->name }}<br>
-    Descrição: {{ $classe->description }}<br>
-    Ordem: {{ $classe->order_classe}}<br>
-    Cadastrado em: {{ \Carbon\carbon::parse($classe->created_at)->format('d/m/y H:i:s') }}<br>
-    Editado em: {{ \Carbon\carbon::parse($classe->updated_at)->format('d/m/y H:i:s') }}<br>
+            <dt class="col-sm-3">Nome</dt>
+            <dd class="col-sm-9">{{ $classe->name }}</dd>
+
+            <dt class="col-sm-3">Cadastrado em:</dt>
+            <dd class="col-sm-9">{{ \Carbon\carbon::parse($classe->created_at)->format('d/m/y H:i:s') }}</dd>
+
+            <dt class="col-sm-3">Editado em:</dt>
+            <dd class="col-sm-9">{{ \Carbon\carbon::parse($classe->updated_at)->format('d/m/y H:i:s') }}</dd>
+
+        </dl>
+    </div>
+
+    <div class="d-flex flex-row">
+        <a href="{{ route('classe.edit', ['classe' => $classe->id]) }}" class="btn btn-primary btn-sm me-1 mt-1 mt-md-0">Editar</a>
+
+        <form action="{{ route('classe.destroy', ['classe' => $classe->id]) }}" method="POST">
+            @csrf
+            @method('delete')
+            <button class="btn btn-danger btn-sm mt-1 mt-md-0" type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
+        </form>
+    </div>
+
+</div>
+
 @endsection
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,22 +1,57 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>Detalhes do curso</h2>
+<div class="container-fluid px-4">
 
-    <a href="{{ route('courses.index') }}">Listar</a><br>
-    <a href="{{ route('courses.edit', ['course' => $course->id]) }}">Editar</a><br><br>
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Curso {{ $course->name }}</h2>
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item active">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">Curso</li>
+        </ol>
+    </div>
 
-    <x-alert />
+    <div class="card mb-4 hstack gap-2">
+        <span class="card-header">Visualizar</span>
+        <span class="ms-auto">
+            <a href="{{ route('classe.index', ['course' => $course->id]) }}" class="btn btn-info btn-sm">Aulas</a>
+            <a href="{{ route('courses.index') }}" class="btn btn-secondary btn-sm">Listar</a>
+        </span>
+    </div>
 
-    <form action="{{ route('courses.destroy', ['course' => $course->id]) }}" method="POST">
-        @csrf
-        @method('delete')
-        <button type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
-    </form>
+    <div class="card-body">
+        <x-alert />
 
-    Id: {{ $course->id }}<br>
-    Nome: {{ $course->name }}<br>
-    Preço: {{ 'R$ ' . number_format($course->price, 2, ',' , '.') }}<br>
-    Cadastrado em: {{ \Carbon\carbon::parse($course->created_at)->format('d/m/y H:i:s') }}<br>
-    Editado em: {{ \Carbon\carbon::parse($course->updated_at)->format('d/m/y H:i:s') }}<br>
+        <dl class="row">
+            <dt class="col-sm-3">Id</dt>
+            <dd class="col-sm-9">{{ $course->id }}</dd>
+
+            <dt class="col-sm-3">Preço</dt>
+            <dd class="col-sm-9">{{ 'R$ ' . number_format($course->price, 2, ',' , '.') }}</dd>
+
+            <dt class="col-sm-3">Cadastrado em:</dt>
+            <dd class="col-sm-9">{{ \Carbon\carbon::parse($course->created_at)->format('d/m/y H:i:s') }}</dd>
+
+            <dt class="col-sm-3">Editado em:</dt>
+            <dd class="col-sm-9">{{ \Carbon\carbon::parse($course->updated_at)->format('d/m/y H:i:s') }}</dd>
+
+        </dl>
+    </div>
+    <div class="d-flex flex-row">
+        <a href="{{ route('courses.edit', ['course' => $course->id]) }}" class="btn btn-primary btn-sm me-1 mt-1 mt-md-0">Editar</a>
+
+        <form action="{{ route('courses.destroy', ['course' => $course->id]) }}" method="POST">
+            @csrf
+            @method('delete')
+            <button class="btn btn-danger btn-sm mt-1 mt-md-0" type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
+        </form>
+    </div>
+
+</div>
+    {{-- Exibir a paginaçã --}}
+    {{-- {{ $courses->links() }} --}}
 @endsection
+
+

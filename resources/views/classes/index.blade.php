2de7a241b28d8fcb68204642f8138b6bac4b70ca
Rubens <rubens.meirelles@postalsaude.com.br>
2024-11-29 16:02:28 -0300

    Implementado o layout na página de listar aulas

--- resources/views/classes/index.blade.php
+++ resources/views/classes/index.blade.php
@@ -1,38 +1,66 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>Listar aulas</h2>
+<div class="container-fluid px-4">
+    <div class="mb-1 hstack gap-2">
+        <h2>Aulas</h2>
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item active">
+                <a href="#" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{ route('courses.index') }}" class="text-decoration-none btn btn-warning btn-sm">Cursos</a>
+            </li>
+        </ol>
+    </div>
 
-    <a href="{{ route('courses.index') }}">
-        <button>Cursos</button>
-    </a><br><br>
+    <div class="card mb-4 hstack gap-2">
+        <span class="card-header">Listar</span>
+        <span class="ms-auto">
+            <a href="{{ route('classe.create', ['course' => $course->id]) }}" class="btn btn-success btn-sm">Cadastrar</a>
+        </span>
+    </div>
 
-    <a href="{{ route('classe.create', ['course' => $course->id]) }}">
-        <button>Cadastrar</button>
-    </a><br><br>
+    <div class="card-body">
+        <x-alert />
 
-<x-alert />
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th class="d-none d-md-table-cell">Id</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th class="text-center">Ações</th>
+                  </tr>
+            </thead>
 
-    @forelse ($classes as $classe)
-        Id: {{ $classe->id }}<br>
-        Nome: {{ $classe->name }}<br>
-        Descrição: {{ $classe->description }}<br>
-        Curso: {{ $classe->course->name }}<br><br>
-        <a href="{{ route('classe.edit', ['classe' => $classe->id]) }}"><button type="submit">Editar</button></a><br><br>
-        <a href="{{ route('classe.show', ['classe' => $classe->id]) }}"><button type="submit">Visualizar</button></a><br><br>
+            <tbody>
+                @forelse ($classes as $classe)
+                    <tr>
+                        <td class="d-none d-md-table-cell">{{ $classe->id }}</td>
+                        <td>{{ $classe->name }}</td>
+                        <td>{{ $classe->description }}</td>
+                        <td class="d-md-flex flex-row justify-content-center">
 
-        <form action="{{ route('classe.destroy', ['classe' => $classe->id]) }}" method="POST">
-            @csrf
-            @method('delete')
-            <button type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
-        </form>
+                            <a href="{{ route('classe.show', ['classe' => $classe->id]) }}" class="btn btn-info btn-sm me-1 mt-1 mt-md-0">Visualizar</a>
+                            <a href="{{ route('classe.edit', ['classe' => $classe->id]) }}" class="btn btn-primary btn-sm me-1 mt-1 mt-md-0">Editar</a>
 
-        <hr>
-    @empty
-        <p style="color: #f00">Nenhuma aula encontrada!</p>
-    @endforelse
+                            <form action="{{ route('classe.destroy', ['classe' => $classe->id]) }}" method="POST">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-danger btn-sm mt-1 mt-md-0" type="submit" onclick="return confirm('Coonfirma a exclusão do registro?')">Apagar</button>
+                            </form>
+                        </td>
+                    </tr>
 
-    {{-- Exibir a paginaçã --}}
-    {{-- {{ $courses->links() }} --}}
+                    @empty
+                    <div class="alert alert-danger" role="alert">
+                        Nenhuma aula encontrada!
+                    </div>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
 

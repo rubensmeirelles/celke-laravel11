902dcf33f1d7e97efbfcfb428ee3837448d67c84
Rubens <rubens.meirelles@postalsaude.com.br>
2024-12-05 20:10:54 -0300

    Implementando o método de login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index() {
         return view('login.index');
+    }
+
+    public function loginProcess(LoginRequest $request){
+
+        $request->validated();
+
+        //Validar os dados
+        $authenticated = Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+
+        if(!$authenticated){
+            //Redireciona para o login novamente
+            return back()->withInput()->with('error', 'E-mail ou senha inválidos!');
+        }
+        //Redireciona para a página pruncipal
+        return redirect()->route('dashboard.index');
     }
 }

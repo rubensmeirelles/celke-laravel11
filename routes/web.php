193b3d4441ab9681dd02468b8d9dd1487e0c4d93
Rubens <rubens.meirelles@postalsaude.com.br>
2024-12-05 22:20:11 -0300

    Implementando os métodos do perfil - editar, visualizar, de acordo com o usuário logado

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 
 // Route::get('/', function () {
@@ -20,36 +21,43 @@ Route::post('/store-user-login', [LoginController:: class, 'store'])->name('logi
 
 Route::group(['middleware' => 'auth'], function() {
 
-// Dashboard
-Route::get('/index-dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    // Dashboard
+    Route::get('/index-dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
-// Cursos
-Route::get('/index-course', [CourseController:: class, 'index'])->name('courses.index');
-Route::get('/show-course/{course}', [CourseController:: class, 'show'])->name('courses.show');
-Route::get('/create-course', [CourseController:: class, 'create'])->name('courses.create');
-Route::post('/store-course', [CourseController:: class, 'store'])->name('courses.store');
-Route::get('/edit-course/{course}', [CourseController:: class, 'edit'])->name('courses.edit');
-Route::put('/update-course/{course}', [CourseController:: class, 'update'])->name('courses.update');
-Route::delete('/destroy-course/{course}', [CourseController:: class, 'destroy'])->name('courses.destroy');
+    //Perfil
+    Route::get('/show-profile}', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/edit-profile}', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/update-profile}', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/edit-profile-password}', [ProfileController::class, 'editPassword'])->name('profile.edit-password');
+    Route::put('/update-profile-password}', [ProfileController::class, 'updatePassword'])->name('profile.update-password');
 
-// Aulas
-Route::get('/index-classe/{course}', [ClasseController::class, 'index'])->name('classe.index');
-Route::get('/show-classe/{classe}', [ClasseController::class, 'show'])->name('classe.show');
-Route::get('/create-classe/{course}', [ClasseController::class, 'create'])->name('classe.create');
-Route::post('/store-classe', [ClasseController::class, 'store'])->name('classe.store');
-Route::get('/edit-classe/{classe}', [ClasseController::class, 'edit'])->name('classe.edit');
-Route::put('/update-classe/{classe}', [ClasseController::class, 'update'])->name('classe.update');
-Route::delete('/destroy-classe/{classe}', [ClasseController:: class, 'destroy'])->name('classe.destroy');
+    // Usuários
+    Route::get('/index-user', [UserController::class, 'index'])->name('user.index');
+    Route::get('/show-user/{user}', [UserController::class, 'show'])->name('user.show');
+    Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
+    Route::post('/store-user', [UserController::class, 'store'])->name('user.store');
+    Route::get('/edit-user/{user}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/update-user/{user}', [UserController::class, 'update'])->name('user.update');
+    Route::get('/edit-user-password/{user}', [UserController::class, 'editPassword'])->name('user.edit-password');
+    Route::put('/update-user-password/{user}', [UserController::class, 'updatePassword'])->name('user.update-password');
+    Route::delete('/destroy-user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 
-// Usuários
-Route::get('/index-user', [UserController::class, 'index'])->name('user.index');
-Route::get('/show-user/{user}', [UserController::class, 'show'])->name('user.show');
-Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
-Route::post('/store-user', [UserController::class, 'store'])->name('user.store');
-Route::get('/edit-user/{user}', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/update-user/{user}', [UserController::class, 'update'])->name('user.update');
-Route::get('/edit-user-password/{user}', [UserController::class, 'editPassword'])->name('user.edit-password');
-Route::put('/update-user-password/{user}', [UserController::class, 'updatePassword'])->name('user.update-password');
-Route::delete('/destroy-user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    // Cursos
+    Route::get('/index-course', [CourseController:: class, 'index'])->name('courses.index');
+    Route::get('/show-course/{course}', [CourseController:: class, 'show'])->name('courses.show');
+    Route::get('/create-course', [CourseController:: class, 'create'])->name('courses.create');
+    Route::post('/store-course', [CourseController:: class, 'store'])->name('courses.store');
+    Route::get('/edit-course/{course}', [CourseController:: class, 'edit'])->name('courses.edit');
+    Route::put('/update-course/{course}', [CourseController:: class, 'update'])->name('courses.update');
+    Route::delete('/destroy-course/{course}', [CourseController:: class, 'destroy'])->name('courses.destroy');
+
+    // Aulas
+    Route::get('/index-classe/{course}', [ClasseController::class, 'index'])->name('classe.index');
+    Route::get('/show-classe/{classe}', [ClasseController::class, 'show'])->name('classe.show');
+    Route::get('/create-classe/{course}', [ClasseController::class, 'create'])->name('classe.create');
+    Route::post('/store-classe', [ClasseController::class, 'store'])->name('classe.store');
+    Route::get('/edit-classe/{classe}', [ClasseController::class, 'edit'])->name('classe.edit');
+    Route::put('/update-classe/{classe}', [ClasseController::class, 'update'])->name('classe.update');
+    Route::delete('/destroy-classe/{classe}', [ClasseController:: class, 'destroy'])->name('classe.destroy');
 
 });
